e654ab78e436450e99dd1c6ff244238c30324aa8
Mohamed Ghobashy <ghobashy2@gmail.com>
2020-08-10 03:08:30 +0200

    [FEAT] add init and pay by token function calls

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -16,15 +16,31 @@ import {
   StatusBar,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Header, Colors} from 'react-native/Libraries/NewAppScreen';
+import WeAccept from 'react-native-we-accept';
 
 const App: () => React$Node = () => {
+  useEffect(() => {
+    console.log(WeAccept);
+    WeAccept.initWeAccept({
+      paymentKey:
+        'ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SnVZVzFsSWpvaWFXNXBkR2xoYkNJc0luQnliMlpwYkdWZmNHc2lPakUxTWpZeUxDSmpiR0Z6Y3lJNklrMWxjbU5vWVc1MEluMC55TWN5LUZ6cnFIVC1JeG5QRHZJQjNlNFhyekpLOFpsODkwbDRoSmVOaHRSRl9BNlY0NmJmT3d4aTdyMmkwMFVyQ2wxSlZhVmhvTXlHdng3ZVlwS25jdw==',
+    });
+
+    WeAccept.payWithToken(
+      {
+        token: '12345',
+        maskedPanNumber: 'XXXXXXXXXXXXXX1234',
+      },
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      },
+    );
+  }, []);
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
@@ -40,31 +56,14 @@ const App: () => React$Node = () => {
           )}
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
+              <Text style={styles.sectionTitle}>
+                React Native We Accept Sample project
+              </Text>
               <Text style={styles.sectionDescription}>
                 Edit <Text style={styles.highlight}>App.js</Text> to change this
                 screen and then come back to see your edits.
               </Text>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
           </View>
         </ScrollView>
       </SafeAreaView>
@@ -84,6 +83,7 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.white,
   },
   sectionContainer: {
+    flexDirection: 'column',
     marginTop: 32,
     paddingHorizontal: 24,
   },
