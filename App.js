df5dbe9c0f5af2855dafbcf9ecbe54e28ce4bb12
Mohamed Ghobashy <ghobashy2@gmail.com>
2020-08-13 00:03:46 +0200

    [MAINTENANCE] update project settings for android

--- App.js
+++ App.js
@@ -35,33 +35,33 @@ const App: () => React$Node = () => {
     console.log(WeAccept);
     WeAccept.initWeAccept({
       paymentKey:
-        'ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SnVZVzFsSWpvaWFXNXBkR2xoYkNJc0luQnliMlpwYkdWZmNHc2lPakUxTWpZeUxDSmpiR0Z6Y3lJNklrMWxjbU5vWVc1MEluMC55TWN5LUZ6cnFIVC1JeG5QRHZJQjNlNFhyekpLOFpsODkwbDRoSmVOaHRSRl9BNlY0NmJmT3d4aTdyMmkwMFVyQ2wxSlZhVmhvTXlHdng3ZVlwS25jdw==',
+        'ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmlhV3hzYVc1blgyUmhkR0VpT25zaVptbHljM1JmYm1GdFpTSTZJa05zYVdabWIzSmtJaXdpYkdGemRGOXVZVzFsSWpvaVRtbGpiMnhoY3lJc0luTjBjbVZsZENJNklrVjBhR0Z1SUV4aGJtUWlMQ0ppZFdsc1pHbHVaeUk2SWpnd01qZ2lMQ0ptYkc5dmNpSTZJalF5SWl3aVlYQmhjblJ0Wlc1MElqb2lPREF6SWl3aVkybDBlU0k2SWtwaGMydHZiSE5yYVdKMWNtZG9JaXdpYzNSaGRHVWlPaUpWZEdGb0lpd2lZMjkxYm5SeWVTSTZJa05TSWl3aVpXMWhhV3dpT2lKamJHRjFaR1YwZEdVd09VQmxlR0V1WTI5dElpd2ljR2h2Ym1WZmJuVnRZbVZ5SWpvaUt6ZzJLRGdwT1RFek5USXhNRFE0TnlJc0luQnZjM1JoYkY5amIyUmxJam9pTURFNE9UZ2lMQ0psZUhSeVlWOWtaWE5qY21sd2RHbHZiaUk2SWs1QkluMHNJbVY0Y0NJNk1UVTJPVEkwTWpneU1Dd2ljRzFyWDJsd0lqb2lOREV1TkRJdU1UUTVMakUwT1NJc0ltRnRiM1Z1ZEY5alpXNTBjeUk2TVRBd0xDSnZjbVJsY2w5cFpDSTZNell3TVRneE55d2lhVzUwWldkeVlYUnBiMjVmYVdRaU9qWTNOREVzSW5WelpYSmZhV1FpT2pRM01EVXNJbU4xY25KbGJtTjVJam9pUlVkUUlpd2liRzlqYTE5dmNtUmxjbDkzYUdWdVgzQmhhV1FpT21aaGJITmxmUS5YamNQSGpsSmlsbzZXbW1sNlZxZzNuNDdPVE5FV2RDQlZfVFZxR1l4d21BcGRXVkNsajBjeU1fSTlGNUdPREd1d0htMHZzOTd5MmJ3S3BybENrbWRidw==',
     });
 
-    WeAccept.payWithToken(
-      {
-        token: '12345',
-        maskedPanNumber: 'XXXXXXXXXXXXXX1234',
-      },
-      (response) => {
-        console.log('response', response);
-      },
-      (error) => {
-        console.log('error', error);
-      },
-    );
-
-    // WeAccept.payWithNoToken(
+    // WeAccept.payWithToken(
     //   {
-    //     showSaveCard: true,
+    //     token: '12345',
+    //     maskedPanNumber: 'XXXXXXXXXXXXXX1234',
     //   },
     //   (response) => {
-    //     console.log(response);
+    //     console.log('response', response);
     //   },
     //   (error) => {
-    //     console.log(error);
+    //     console.log('error', error);
     //   },
     // );
+
+    WeAccept.payWithNoToken(
+      {
+        showSaveCard: true,
+      },
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      },
+    );
   }, []);
 
   return (
